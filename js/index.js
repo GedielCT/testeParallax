f1b9f4b216a0d3daf0146ccccfe31de5b43d74df
GedielCT <98960368+GedielCT@users.noreply.github.com>
2025-06-10 14:35:03 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,6 @@
-// comentario
-let botao = document.querySelector(".botao");
-let estaQuebrado = false;
-let contaCliques = 0;
+const togglePerfil = document.getElementById('perfil-toggle');
+const perfilMenu = document.getElementById('perfil-menu');
 
-botao.style.background="blue";
-
-botao.addEventListener("mouseover",e =>{
-    if(estaQuebrado===false)
-        botao.style.background="green";
+togglePerfil.addEventListener('click', () => {
+    perfilMenu.classList.toggle('show');
 });
-
-botao.addEventListener("mouseout",trocaAzul);
-
-function trocaAzul(){
-    if(!estaQuebrado){
-        botao.style.background="blue";
-    }
-}
-
-botao.addEventListener("click",botaoQuebra);
-
-function botaoQuebra(){
-    contaCliques++; // contaCliques=contaCliques+1;
-
-    if(contaCliques>=10){
-        botao.style.background="red";
-        botao.innerHTML="quebrei";
-        estaQuebrado=true;
-    }
-}
