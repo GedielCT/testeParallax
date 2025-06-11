ef538a2fd9395577cad99dae666bf76ecd997e6b
GedielCT <gedielcassemiro@gmail.com>
2025-06-11 16:41:32 -0300

    Adicionado o componente dos conteiner de personagens

---
+++ js/card.js
@@ -0,0 +1,31 @@
+class PersonCard extends HTMLElement {
+  connectedCallback() {
+    const nome = this.getAttribute("nome") || "";
+    const habilidade = this.getAttribute("habilidade") || "";
+    const checked = this.hasAttribute("ativo");
+
+    this.innerHTML = `
+      <div class="card">
+        <div class="row flex-container">
+          <div class="fakeimg inputs">Image</div>
+
+          <div class="right-content">
+            <div class="inputs">
+              <label>Nome:</label>
+              <input type="text" class="input-text" value="${nome}" />
+            </div>
+            <div class="inputs">
+              <label>Habilidade:</label>
+              <input type="text" class="input-text" value="${habilidade}" />
+            </div>
+          </div>
+
+          <div class="checkbox-container">
+            <input type="checkbox" ${checked ? "checked" : ""} />
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+customElements.define("person-card", PersonCard);
