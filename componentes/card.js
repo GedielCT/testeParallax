29fca53e0787aa873f9ba07c0864f475fe6f8784
GedielCT <gedielcassemiro@gmail.com>
2025-06-14 12:19:00 -0300

    Adicionado comentarios e modificado alguns arquivos

--- componentes/card.js
+++ componentes/card.js
@@ -1,30 +1,43 @@
 class FichaCard extends HTMLElement {
   connectedCallback() {
-    const nome = this.getAttribute("nome") || "";
-    const habilidade = this.getAttribute("habilidade") || "";
-    const checked = this.hasAttribute("ativo");
+    const nome = this.getAttribute("nome") || ""; /* Pega o nome que foi passado */
+    const habilidade = this.getAttribute("habilidade") || ""; /* Pega a habilidade que foi passado */
+    const checked = this.hasAttribute("ativo"); /* Pega o status do checkbox que foi passado */
 
     this.innerHTML = `
-      <div class="card">
-        <div class="row flex-container">
-          <div class="fakeimg inputs">Image</div>
+      <!-- CSS cars -->
+      <link rel="stylesheet" href="css/cssEspecifico/card.css">
 
-          <div class="right-content">
-            <div class="inputs">
-              <label>Nome:</label>
-              <input type="text" class="input-text" value="${nome}" />
-            </div>
-            <div class="inputs">
-              <label>Habilidade:</label>
-              <input type="text" class="input-text" value="${habilidade}" />
-            </div>
+      <!-- Card -->
+      <div class="card">
+
+        <!-- Futuramente ira vir uma imagem -->
+        <div class="fakeimg inputs">Image</div>
+
+        <!-- Separa os inputs do restante -->
+        <div class="informacoes">
+
+          <!-- Campo NOME -->
+          <div class="campo">
+            <label>Nome:</label>
+
+            <!-- Mostra o nome que foi passado -->
+            <input type="text" class="input" value="${nome}" />
           </div>
 
-          <div class="checkbox-container">
-            <input type="checkbox" ${checked ? "checked" : ""} />
+          <!-- Campo HABILIDADE -->
+          <div class="campo">
+            <label>Habilidade:</label>
+
+            <!-- Mostra a habilidade que foi passado -->
+            <input type="text" class="input" value="${habilidade}" />
           </div>
         </div>
+
+          <!-- Mostra o status do checkbox que foi passado -->
+          <input type="checkbox" class="checkbox-container" ${checked ? "checked" : ""} />
       </div>
+
     `;
   }
 }
--- componentes/menuPrincipal.js
+++ componentes/menuPrincipal.js
@@ -1,14 +1,26 @@
 class MenuPrincipal extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
-                <div class="topnav">        
+                <!-- CSS menu principal -->
+                <link rel="stylesheet" href="css/cssEspecifico/barraNavegacao.css">
+                
+                <!-- Div da barra de menu -->
+                <div class="topnav">
+                
+                    <!-- Botões da esquerda -->
                     <div class="navegacao">
                         <a href="index.html">Home</a>
                         <a href="usarFicha.html">Usar Ficha</a>
                         <a href="cadastrarFicha.html">Cadastrar Ficha</a>
                     </div>
+
+                    <!-- Botões da direita -->
                     <div class="menu-container">
+
+                        <!-- Botão do menu hamburguer -->
                         <button class="menu-toggle" id="menu-toggle">☰</button>
+
+                        <!-- Botões -->
                         <div class="menu" id="menu">
                             <a href="login.html">Login</a>
                             <a href="singUp.html">Sing Up</a>
@@ -17,9 +29,11 @@ class MenuPrincipal extends HTMLElement {
                     </div>
                 </div>
         `;
-        const toggleMenu = this.querySelector('#menu-toggle');
-        const menu = this.querySelector('#menu');
 
+        const toggleMenu = this.querySelector('#menu-toggle'); /* Defini a class menu-toggle */
+        const menu = this.querySelector('#menu'); /* Defini a class menu */
+
+        /* Quando menu-toggle por clicado vai mostrar menu */
         if (toggleMenu) {
             toggleMenu.addEventListener('click', () => {
                 menu.classList.toggle('show');
